2740a24a8345c17bedee4e8d67818bfd97171f95
mdsibghat28 <mdsibghat28@gmail.com>
2022-01-24 11:55:11 +0530

    folders

---
+++ model/src/main/java/com/sibghat/CPS/CpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sibghat.CPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpsBackendApplication.class, args);
+	}
+
+}
---
+++ model/src/test/java/com/sibghat/CPS/CpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sibghat.CPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
